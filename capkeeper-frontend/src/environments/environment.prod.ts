d45c7179c15ac3622044143e4f96717492a5966a
Eric Spensieri <espensieri@hotmail.com>
2024-10-16 15:15:39 -0400

    Prod Environment updated for Firebase Key

--- capkeeper-frontend/src/environments/environment.prod.ts
+++ capkeeper-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://dolphin-app-f5yuy.ondigitalocean.app'
+    apiUrl: 'https://dolphin-app-f5yuy.ondigitalocean.app',
+    firebase: {
+      apiKey: "AIzaSyBwoVvCfhdsRnRyiPWrgrfqeAwTQcOJ5oI",
+      authDomain: "capkeeper-8824f.firebaseapp.com",
+      projectId: "capkeeper-8824f",
+      storageBucket: "capkeeper-8824f.appspot.com",
+      messagingSenderId: "454606071170",
+      appId: "1:454606071170:web:95d75f6f8121c9f36d1b9b",
+      measurementId: "G-8WCSDM3TBW"
+    }
   };
   
