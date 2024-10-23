042bd61b1117226da8d6cb23fc9b85952cfea564
Eric Spensieri <espensieri@hotmail.com>
2024-10-23 19:57:30 -0400

    Without escape characters

--- capkeeper-backend/src/server.js
+++ capkeeper-backend/src/server.js
@@ -21,7 +21,7 @@ const credentials = {
     type: "service_account",
     project_id: process.env.GOOGLE_PROJECT_ID,
     private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     client_id: process.env.GOOGLE_CLIENT_ID,
     auth_uri: process.env.GOOGLE_AUTH_URI,
